0fc6f4f475ea2b3f769a2a7bf1cc63abde2e8999
Alvin Mites <alvin@mitesdesign.com>
2017-12-11 14:19:40 -0700

    compatibility with django 1.11

--- gmap_admin/fields.py
+++ gmap_admin/fields.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.core import exceptions
 from django import forms
-from django.utils.encoding import smart_unicode, smart_str, force_unicode
 
-from widgets import GoogleMapsWidget
+from gmap_admin.widgets import GoogleMapsWidget
+
 
 def typename(obj):
     """Returns the type of obj as a string. More descriptive and specific than
@@ -13,8 +13,9 @@ def typename(obj):
     else:
         return type(obj).__name__
 
+
 class GeoPt(object):
-    "A geographical point."
+    """A geographical point."""
 
     lat = None
     lon = None
@@ -31,31 +32,35 @@ class GeoPt(object):
     def __len__(self):
         return len(self.__unicode__())
 
-    def _split_geo_point(self, geo_point):
-        "splits the geo point into lat and lon"
+    @staticmethod
+    def _split_geo_point(geo_point):
+        """splits the geo point into lat and lon"""
         try:
             return geo_point.split(',')
         except (AttributeError, ValueError):
             raise exceptions.ValidationError(
-                'Expected a "lat,long" formatted string; received %s (a %s).' %
-            (geo_point, typename(geo_point)))
+                'Expected a "lat,long" formatted string; received {} (a {}).'.format(
+                    geo_point, typename(geo_point)))
 
-    def _validate_geo_range(self, geo_part, range_val):
+    @staticmethod
+    def _validate_geo_range(geo_part, range_val):
         try:
             geo_part = float(geo_part)
             if abs(geo_part) > range_val:
                 raise exceptions.ValidationError(
-                'Must be between -%s and %s; received %s' % (range_val, range_val, geo_part)
-            )
+                    'Must be between -%s and %s; received %s' % (range_val, range_val, geo_part)
+                )
         except (TypeError, ValueError):
             raise exceptions.ValidationError(
                 'Expected float, received %s (a %s).' % (geo_part, typename(geo_part))
             )
         return geo_part
 
+
 class GoogleMapsFormField(forms.CharField):
     pass
-          
+
+
 class GeoLocationField(models.CharField):
     """
     A geographical point, specified by floating-point latitude and longitude
@@ -76,7 +81,6 @@ class GeoLocationField(models.CharField):
         super(GeoLocationField, self).__init__(*args, **kwargs)
 
     def to_python(self, value):
-        print "asdf:%s"%value
         if isinstance(value, GeoPt):
             return value
         return GeoPt(value)
@@ -99,17 +103,18 @@ class GeoLocationField(models.CharField):
             raise TypeError('Lookup type %r not supported.' % lookup_type)
 
     def value_to_string(self, obj):
-        print obj
         value = self._get_val_from_obj(obj)
         return self.get_db_prep_value(value)
 
     def formfield(self, **kwargs):
-        defaults = {'widget':GoogleMapsWidget}
+        defaults = {'widget': GoogleMapsWidget, }
         kwargs.update(defaults)
         return super(GeoLocationField, self).formfield(form_class=GoogleMapsFormField, **kwargs)
-        
+
+
+# legacy support for south
 try:
- 	from south.modelsinspector import add_introspection_rules
-	add_introspection_rules([], ["^gmap_admin\.fields\.GeoLocationField"])
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^gmap_admin\.fields\.GeoLocationField"])
 except ImportError:
-	pass
+    pass
--- gmap_admin/widgets.py
+++ gmap_admin/widgets.py
@@ -1,61 +1,64 @@
 from django.conf import settings
 from django.forms import widgets
-from django.utils.encoding import force_unicode
+from django.utils.encoding import force_str
 from django.utils.safestring import mark_safe
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
+
 from gmap_admin import settings as gmap_settings
 
 
 class GoogleMapsWidget(widgets.HiddenInput):
-	
-	class Media:
-		js = [
-			'http://maps.google.com/maps/api/js?sensor=false',
-			'gmap_admin/js/google-maps-admin.js',
-			]
 
-	def render(self, name, value, attrs=None):
-		if value is None or value == ',':
-			value = ""
-			center_lng = gmap_settings.DEFAULT_LNG
-			center_lat = gmap_settings.DEFAULT_LAT
-		final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
-		if len(value) > 1:
-			final_attrs['value'] = force_unicode(self._format_value(value))
-			center_lat,center_lng = final_attrs['value'].split(',')
-		else:
-			center_lat = gmap_settings.DEFAULT_LAT
-			center_lng = gmap_settings.DEFAULT_LNG
-		html = u"""
-					<input%(attrs)s />
-					<div class="map_canvas_wrapper" style="display:inline-block;">
-						<div id="%(map_id)s" style="width:%(width)spx;height:%(height)spx"></div>
-						<script>
-							django.jQuery(function($) {
-								$("#%(field_id)s").gmapAdmin({
-										'zoom': %(zoom)s
-										,'lat': '%(center_lat)s'
-										,'lng': '%(center_lng)s'
-										,'map_elem': '#%(map_id)s'
-										,'delete_elem': '#%(delete_id)s'
-										,'add_elem': '#%(add_id)s'
-								});
-							});
-						</script>
-					</div>
-					<p><a id="%(delete_id)s" href="javascript:void(0)">Remove Marker</a></p>
-					<p><a id="%(add_id)s" href="javascript:void(0)">Add Marker</a></p>
-					<p class="help">Double click to zoom in and center on a location. Right click to set the marker on a position. You can also drag and drop the marker.</p>
-				""" % { 
-						'field_id': attrs['id'],
-						'delete_id': 'map_delete_%s'%attrs['id'],
+    class Media:
+        js = [
+            'http://maps.google.com/maps/api/js?sensor=false',
+            'gmap_admin/js/google-maps-admin.js',
+            ]
+
+    def render(self, name, value, attrs=None):
+        if value is None or value == ',':
+            value = ""
+            center_lng = gmap_settings.DEFAULT_LNG
+            center_lat = gmap_settings.DEFAULT_LAT
+        #     def build_attrs(self, base_attrs, extra_attrs=None):
+        final_attrs = self.build_attrs(attrs, {'type': self.input_type, 'name': name, })
+        # final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        if len(value) > 1:
+            final_attrs['value'] = force_str(self._format_value(value))
+            center_lat,center_lng = final_attrs['value'].split(',')
+        else:
+            center_lat = gmap_settings.DEFAULT_LAT
+            center_lng = gmap_settings.DEFAULT_LNG
+        html = u"""
+                    <input%(attrs)s />
+                    <div class="map_canvas_wrapper" style="display:inline-block;">
+                        <div id="%(map_id)s" style="width:%(width)spx;height:%(height)spx"></div>
+                        <script>
+                            django.jQuery(function($) {
+                                $("#%(field_id)s").gmapAdmin({
+                                        'zoom': %(zoom)s
+                                        ,'lat': '%(center_lat)s'
+                                        ,'lng': '%(center_lng)s'
+                                        ,'map_elem': '#%(map_id)s'
+                                        ,'delete_elem': '#%(delete_id)s'
+                                        ,'add_elem': '#%(add_id)s'
+                                });
+                            });
+                        </script>
+                    </div>
+                    <p><a id="%(delete_id)s" href="javascript:void(0)">Remove Marker</a></p>
+                    <p><a id="%(add_id)s" href="javascript:void(0)">Add Marker</a></p>
+                    <p class="help">Double click to zoom in and center on a location. Right click to set the marker on a position. You can also drag and drop the marker.</p>
+                """ % {
+                        'field_id': attrs['id'],
+                        'delete_id': 'map_delete_%s'%attrs['id'],
                         'add_id' : 'map_add_%s'%attrs['id'],
-						'map_id': 'map_%s'%attrs['id'],
-						'attrs': flatatt(final_attrs),
-						'height': gmap_settings.HEIGHT,
-						'width': gmap_settings.WIDTH,
-						'zoom': gmap_settings.ZOOM,
-						'center_lng': center_lng,
-						'center_lat': center_lat,
-					}
-		return mark_safe(html)
+                        'map_id': 'map_%s'%attrs['id'],
+                        'attrs': flatatt(final_attrs),
+                        'height': gmap_settings.HEIGHT,
+                        'width': gmap_settings.WIDTH,
+                        'zoom': gmap_settings.ZOOM,
+                        'center_lng': center_lng,
+                        'center_lat': center_lat,
+                    }
+        return mark_safe(html)
